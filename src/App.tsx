71605e1ab4d1c28ced03cc96e6ba91762607cb1b
Khrunev Denis <deniskhrunev@gmail.com>
2024-05-16 15:26:52 +0600

    Созадана логика для изменения значений в компоненты при нажатии на конпку. Задание выполнено

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,26 @@ import {useState} from 'react';
 import Ball from './components/Ball/Ball';
 
 const App = () => {
-  const [numberBall] = useState([5, 11, 16, 23, 32]);
+  const [numberBall, setNumberBall] = useState([5, 11, 16, 23, 32]);
 
+  const createNewNumber = () => {
+      const newNumber: number[] = [];
+      while (newNumber.length < 5) {
+          const randomNumber = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+          if(!newNumber.includes(randomNumber)) {
+              newNumber.push(randomNumber);
+          }
+      }
+      newNumber.sort((a, b) => a - b);
+      setNumberBall(newNumber);
+  };
 
   return (
     <>
-      <button className="btn">New numbers</button>
-      <Ball number={numberBall[0]}/>
-      <Ball number={numberBall[1]}/>
-      <Ball number={numberBall[2]}/>
-      <Ball number={numberBall[3]}/>
-      <Ball number={numberBall[4]}/>
+      <button className="btn" onClick={createNewNumber}>New numbers</button>
+      {numberBall.map((num, index) => (
+          <Ball key={index} number={num} />
+      ))}
     </>
   );
 };
